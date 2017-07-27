dbde0ee56b43bd7f3cea4d75aa4075b59679180e
Lucas Simon <luke5227@gmail.com>
2017-07-27 02:48:03 -0700

    Support for selectors and updating classes every 6 hours

--- fetchclasses.js
+++ fetchclasses.js
@@ -1,18 +1,16 @@
 const request = require('request');
 const fs = require('fs');
 
-if (process.argv.length < 3) {
+/*if (process.argv.length < 3) {
   console.log('Usage: node fetchclasses.js <term>');
   console.log('Term is in the format 2{$year}{$quarter} where $year is the last two digits of the current year');
   console.log('and $quarter is either 0, 2, 4, or 8 for winter, spring, summer, and fall respectively.');
   console.log('E.g. 2178 for Fall of 2017');
   process.exit(1);
-}
+}*/
 
-const classes = {};
-
-function fetchPages(term) {
-  console.log('Fetching for term '+term);
+function fetchTerm(term, cb) {
+  const classes = {};
   request.post('https://pisa.ucsc.edu/class_search/index.php', {form: {action: 'results', 'binds[:term]': term, rec_dur: 50000} }, (err, http, body) => {
     const classNumbers = [];
     const re = /id="class_title_(\d+)/g;
@@ -23,21 +21,23 @@ function fetchPages(term) {
       if (m && classNumbers[classNumbers.length-1] != m[1]) classNumbers.push(m[1].replace(/&amp;/g, '&'));
     } while (m);
 
-    console.log('Got '+classNumbers.length+' classes for term '+term);
-    fetchClasses(term, classNumbers, allDone);
+    fetchClasses(term, classNumbers, classes, cb);
   });
 }
 
-function fetchClasses(term, classNumbers, cb) {
+function fetchClasses(term, classNumbers, classes, cb) {
   let doneCount = 0;
   const done = () => {
     doneCount++;
-    console.log(`[Term ${term}] ${Math.round(doneCount/classNumbers.length*100)}% complete`);
+    if (doneCount % 200 == 0) console.log(`[Term ${term} class fetch] ${Math.round(doneCount/classNumbers.length*100)}% complete`);
     if (classNumbers[nextToFetch]) {
       fetch(classNumbers[nextToFetch]);
       nextToFetch++;
     }
-    if (doneCount == classNumbers.length) cb();
+    if (doneCount == classNumbers.length) {
+      console.log(`[Term ${term} class fetch] Finished.`);
+      cb(classes);
+    }
   }
 
   let nextToFetch = 20;
@@ -110,17 +110,13 @@ function parseClass(classBody) {
   return classData;
 }
 
-let doneCount = 0;
-function allDone() {
-  doneCount++;
-  if (doneCount == process.argv.length - 2) {
-    fs.writeFileSync('./classdata.json', JSON.stringify(classes));
-    console.log('Done and saved!');
-  }
-}
-
 function convertToCamelCase(str) {
   return str.toLowerCase().split(' ').map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
 }
 
-process.argv.slice(2).forEach(term => fetchPages(term));
+module.exports = function(term, cb) {
+  fetchTerm(term, classes => {
+    fs.writeFileSync('./classdata.json', JSON.stringify({ lastUpdated: Date.now(), classes: classes }));
+    if (cb) cb(classes);
+  });
+}
--- index.js
+++ index.js
@@ -1,41 +1,103 @@
 const Discord = require('discord.js');
 const config = require('config');
 const fs = require('fs');
+const emojiLib = require('node-emoji');
+const fetchclasses = require('./fetchclasses');
 
 const client = new Discord.Client();
 
-let classes;
-const classStrings = {};
+let classes = {};
+let classStrings = {};
+let lastUpdated = 0;
 
-try {
-  classes = JSON.parse(fs.readFileSync('./classdata.json'));
-} catch (e) {
-  console.log('You need to fetch the classes! Run fetchclasses.js');
-  process.exit(1);
+const EMOJI_MAPPING = {
+  regional_indicator_a: '🇦',
+  regional_indicator_b: '🇧',
+  regional_indicator_c: '🇨',
+  regional_indicator_d: '🇩',
+  regional_indicator_e: '🇪',
+  regional_indicator_f: '🇫',
+  regional_indicator_g: '🇬',
+  regional_indicator_h: '🇭',
+  regional_indicator_i: '🇮',
+  regional_indicator_j: '🇯',
+  regional_indicator_k: '🇰',
+  regional_indicator_l: '🇱',
+  regional_indicator_m: '🇲',
+  regional_indicator_n: '🇳',
+  regional_indicator_o: '🇴',
+  regional_indicator_p: '🇵',
+  regional_indicator_q: '🇶',
+  regional_indicator_r: '🇷',
+  regional_indicator_s: '🇸',
+  regional_indicator_t: '🇹',
+  regional_indicator_u: '🇺',
+  regional_indicator_v: '🇻',
+  regional_indicator_w: '🇼',
+  regional_indicator_x: '🇽',
+  regional_indicator_y: '🇾',
+  regional_indicator_z: '🇿',
+  zero: '0⃣',
+  one: '1⃣',
+  two: '2⃣',
+  three: '3⃣',
+  four: '4⃣',
+  five: '5⃣',
+  six: '6⃣',
+  seven: '7⃣',
+  eight: '8⃣',
+  nine: '9⃣',
+  keypad_ten: '🔟'
 }
 
-Object.keys(classes).forEach(classId => {
-  const classData = classes[classId];
-  if (!classStrings[classData.name.toLowerCase()]) classStrings[classData.name.toLowerCase()] = classId;
-  const nameArr = classData.fullName.split(' ').slice(0, 4);
+try {
+  const info = JSON.parse(fs.readFileSync('./classdata.json'));
+  lastUpdated = info.lastUpdated;
+  classes = info.classes;
+} catch (e) {
+  // Will fetch classes
+}
 
-  classStrings[nameArr.join(' ').toLowerCase()] = classId;
-  classStrings[nameArr.join(' ').toLowerCase().replace(' -', '')] = classId;
-
-  if (parseInt(nameArr[3]) < 10) {
-    classStrings[classData.name.toLowerCase()+' - '+nameArr[3].slice(1)] = classId;
-    classStrings[classData.name.toLowerCase()+' '+nameArr[3].slice(1)] = classId;
+setTimeout(() => {
+  const gotClasses = returnedClasses => {
+    classes = returnedClasses;
+    createClassStrings();
   }
-});
+  fetchclasses(config.get('classSearch.term'), gotClasses);
+  setInterval(() => {
+    fetchclasses(config.get('classSearch.term'), gotClasses);
+  }, config.get('classSearch.interval')*1000);
+}, Math.max(config.get('classSearch.interval')*1000 - (Date.now() - lastUpdated), 0));
+
+function createClassStrings() {
+  classStrings = {};
+  Object.keys(classes).forEach(classId => {
+    const classData = classes[classId];
+    if (!classStrings[classData.name.toLowerCase()]) classStrings[classData.name.toLowerCase()] = classId;
+    const nameArr = classData.fullName.split(' ').slice(0, 4);
+
+    classStrings[nameArr.join(' ').toLowerCase()] = classId;
+    classStrings[nameArr.join(' ').toLowerCase().replace(' -', '')] = classId;
+
+    if (parseInt(nameArr[3]) < 10) {
+      classStrings[classData.name.toLowerCase()+' - '+nameArr[3].slice(1)] = classId;
+      classStrings[classData.name.toLowerCase()+' '+nameArr[3].slice(1)] = classId;
+    }
+  });
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+  client.channels.find('id', config.get('selectorChannel')).fetchMessages()
+    .then(() => console.log('Got selectorChannel messages!'))
+    .catch(e => console.log('Error getting selectorChannel messages', e));
 });
 
 client.on('message', msg => {
+  console.log(msg.author+': '+msg.content);
   if (msg.content.indexOf('!class') == 0) {
     const match = msg.content.match(/!class (.+)/);
-    if (!match) msg.reply(`Invalid usage! Try \`!class <class name or number>\` (e.g. \`!class ams 3\`)`)
+    if (!match) return msg.reply(`Invalid usage! Try \`!class <class name or number>\` (e.g. \`!class ams 3\`)`)
     const classData = classes[match[1]] || classes[classStrings[match[1].toLowerCase()]];
     if (!classData) msg.reply(`Could not find that class!`);
     else msg.channel.send('', {embed: getClassEmbed(classData)});
@@ -44,6 +106,24 @@ client.on('message', msg => {
     msg.channel.send('', {embed: getClassEmbed(classData)});
   } else if (msg.content == '!github') {
     msg.reply('https://github.com/demipixel/slugbot');
+  } else if (msg.content.indexOf('!selector') == 0) {
+    const match = msg.content.match(/!selector ([^ ]+)( forever)?/);
+    if (!match) return msg.reply('Invalid usage! Try `!selector <name of selector>`');
+    const selectorType = match[1];
+    if (!config.get('emojis')[selectorType]) return msg.reply('Invalid selector type!');
+    let message = config.get('messages.emojiSelectors')[selectorType]+'\n';
+    message += Object.keys(config.get('emojis')[selectorType]).map(emoji => {
+      return (msg.guild.emojis.find('name', emoji) || ':'+emoji+':')+' '+config.get('emojis')[selectorType][emoji];
+    }).join('\n');
+    msg.channel.send(message).then(msgObj => {
+      Object.keys(config.get('emojis')[selectorType]).forEach((emoji, index) => {
+        const emote = msg.guild.emojis.find('name', emoji) || EMOJI_MAPPING[emoji] || emojiLib.get(emoji);
+        setTimeout(() => msgObj.react(emote), index*500);
+      });
+      if (!match[2] || !msg.author.id != '125696820901838849') setTimeout(() => msgObj.react('🗑'), Object.keys(config.get('emojis')[selectorType]).length*500);
+    }).catch(err => {
+      console.log('Error sending message', err);
+    });
   }
 });
 
@@ -68,6 +148,64 @@ function getClassEmbed(classData) {
     ],
     footer: { text: 'Information from http://pisa.ucsc.edu/class_search/' }
   }
+}
+
+client.on('messageReactionAdd', (reactionObj, user) => {
+  if (!reactionObj.message.guild) return;
+  if (user == client.user) return;
+
+  if (reactionObj.emoji.name == '🗑' && reactionObj.me) {
+    reactionObj.message.delete();
+    return;
+  }
+
+  const {roleName, type} = getRoleFromReaction(reactionObj);
+  const emojiToRole = config.get('emojis')[type];
+
+  if (roleName) {
+    const allRoles = Object.values(emojiToRole);
+    reactionObj.message.guild.fetchMember(user).then(member => {
+      // Remove roles relating to message
+      member.removeRoles(member.roles.filterArray(role => allRoles.includes(role.name)));
+      setTimeout(() => member.addRole(reactionObj.message.guild.roles.find('name', roleName)), 100);
+    }).catch(err => {
+      console.log(err);
+      user.send('There was an error getting your member object! Could not change roles.');
+    });
+  }
+});
+
+client.on('messageReactionRemove', (reactionObj, user) => {
+  if (!reactionObj.message.guild) return;
+  if (user == client.user) return;
+  const {roleName, type} = getRoleFromReaction(reactionObj);
+  const emojiToRole = config.get('emojis')[type];
+
+  if (roleName) {
+    reactionObj.message.guild.fetchMember(user).then(member => {
+      member.removeRole(reactionObj.message.guild.roles.find('name', roleName));
+    }).catch(err => {
+      console.log(err);
+      user.send('There was an error getting your member object! Could not change roles.');
+    });
+  }
+});
+
+function getRoleFromReaction(reactionObj) {
+  const emojiSelectors = config.get('messages.emojiSelectors');
+  const emojiSelectorKeys = Object.keys(emojiSelectors);
+
+  for (let i = 0; i < emojiSelectorKeys.length; i++) {
+    const key = emojiSelectorKeys[i]
+    const text = emojiSelectors[key];
+    if (reactionObj.message.content.slice(0, text.length) == text) {
+      let name = reactionObj.emoji.name;
+      if (Object.values(EMOJI_MAPPING).includes(name)) name = Object.keys(EMOJI_MAPPING)[Object.values(EMOJI_MAPPING).indexOf(name)];
+      return { roleName: config.get('emojis')[key][name], type: key };
+    }
+  }
+
+  return {role: null, type: null};
 }
 
 client.login(config.get('discord.token'));
