a575d5deca44959c4fa1bfa56a96e8cd2a80ca83
Lucas Simon <luke5227@gmail.com>
2020-03-12 23:58:29 -0700

    Virus mutated

--- virus.js
+++ virus.js
@@ -5,7 +5,7 @@ module.exports = {
 
   message: function(client, msg) {
     if (previousInfected[msg.channel.name] && !isInfected(msg.member)) {
-      if (Math.random() < 0.005) {
+      if (Math.random() < 0.01) {
         msg.member.addRole(
           client.guilds.first().roles.find(role => role.name === 'Infected'),
         );
